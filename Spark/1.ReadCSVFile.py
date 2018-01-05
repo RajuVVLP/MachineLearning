fb8eb06b599be40ab7f47ae7320aad7f7c71db0e
Sreeni Jilla <cnu.jilla@gmail.com>
2018-01-05 17:40:57 +0530

    Uploaded sample python code to read csv file.

---
+++ Spark/1.ReadCSVFile.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 12:52:15 2017
+
+@author: ravikishore.ch
+"""
+
+
+import os
+import inspect
+import sys
+
+
+# Initiate Spark context.
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+#
+SPARK_HOME=os.path.dirname(inspect.getfile(SparkContext))
+sys.path.insert(0,os.path.join(SPARK_HOME,"python"))
+sys.path.insert(0,os.path.join(SPARK_HOME,"python","lib"))
+sys.path.insert(0,os.path.join(SPARK_HOME,"python","lib","pyspark.zip"))
+sys.path.insert(0,os.path.join(SPARK_HOME,"python","lib","py4j-0.9-src.zip"))
+
+# Configure Spark Settings
+conf=SparkConf()
+conf.set("spark.executor.memory", "1g")
+conf.set("spark.cores.max", "2")
+conf.setAppName("spark")
+
+
+
+# Initialize SparkContext. 
+sc = SparkContext('local', conf=conf)
+
+lines=sc.textFile("D:/Samples/Python/DataScience/House/house_test.csv")
+
+print (lines.count())
+
+
+sc.stop()
